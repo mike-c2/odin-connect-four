f5bdd19d25f358b6762bd6e2a50f61117ec55e28
Mike Costa <mcosta1@gmail.com>
2024-02-27 23:40:06 -0500

    Removed direct references to the Node class
    The Node class should not be present here since it exists outside of
    NodeArray.  It was making the unit tests impure with the dependency.

--- spec/node_array_spec.rb
+++ spec/node_array_spec.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-require_relative '../lib/node'
 require_relative '../lib/node_array'
 
 describe NodeArray do
   subject(:node_array) { described_class.new }
 
   describe '#to_s' do
-    context "when there's a NodeArray of Nodes" do
+    context "when there's a NodeArray of characters" do
       before do
-        node_array.push(Node.new('A'))
-        node_array.push(Node.new(' '))
-        node_array.push(Node.new('V'))
-        node_array.push(Node.new('a'))
-        node_array.push(Node.new('l'))
-        node_array.push(Node.new('u'))
-        node_array.push(Node.new('e'))
+        # Simulating Nodes in the NodeArray
+        node_array.push('A')
+        node_array.push(' ')
+        node_array.push('V')
+        node_array.push('a')
+        node_array.push('l')
+        node_array.push('u')
+        node_array.push('e')
       end
 
-      it 'return string concatenation of all Node values' do
+      it 'return string concatenation of all values' do
         value = 'A Value'
         result = node_array.to_s
 
