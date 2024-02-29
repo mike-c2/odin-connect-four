cf02543f670b625b991bcee096c1b3a326311e2e
Mike Costa <mcosta1@gmail.com>
2024-02-28 20:46:27 -0500

    Add Grid class and unit tests
    Is a 2-dimensional array, but with row, column, and diagonal views.  The
    different views will be needed to check for the 4 in a row winning
    condition.  Every possible line (up, down, and diagonals) is represented
    as an array (stored in all_majors), so will just need to loop through
    that to see if any 4 in a row is present.

---
+++ lib/grid.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require_relative '../lib/node'
+require_relative '../lib/node_array'
+
+##
+# This class is a grid of Nodes.
+#
+# It is built as an array of NodeArrays in row-major,
+# but also has column-major and diagonal views.
+# Each of these views will hold the same Node
+# references.
+#
+# For example, a single node in a particular row, is
+# also referenced by a column array and two diagonal
+# arrays (uphill and downhill). If the node's value
+# changes in one of those, then it changes in all.
+#
+# All arrays are frozen.
+class Grid
+  DEFAULT_ROWS = 6
+  DEFAULT_COLUMNS = 7
+
+  # See https://en.wikipedia.org/wiki/Row-_and_column-major_order
+  # for meaning of major.
+  #
+  # Each of these are just different views of the
+  # same grid. all_major is all of the other majors
+  # combined into one array.
+  #
+  # row_major is the main view (grid is initially
+  # built this way); column and diagonal majors are
+  # derived from that.
+  attr_reader :row_major, :column_major, :uphill_diagonal_major, :downhill_diagonal_major, :all_majors
+
+  def initialize(initial_value = '', num_rows = DEFAULT_ROWS, num_columns = DEFAULT_COLUMNS)
+    @row_major = build_row_major(initial_value, num_rows, num_columns)
+    @column_major = derive_column_major(row_major)
+    @uphill_diagonal_major = derive_uphill_diagonal_major(row_major)
+    @downhill_diagonal_major = derive_downhill_diagonal_major(row_major)
+
+    @all_majors = row_major + column_major + uphill_diagonal_major + downhill_diagonal_major
+  end
+
+  class << self
+    def build_row_major(initial_value = '', rows = DEFAULT_ROWS, columns = DEFAULT_COLUMNS)
+      row_major = Array.new(rows) { NodeArray.new(columns).freeze { Node.new(initial_value) } }
+
+      row_major.freeze
+    end
+
+    def derive_column_major(node_arrays)
+      column_major = node_arrays.transpose.map(&:freeze)
+
+      column_major.freeze
+    end
+
+    def derive_uphill_diagonal_major(node_arrays)
+      uphill_diagonal_major = []
+
+      # Go down the left side of the table (2-d array)
+      (0...node_arrays.length).each do |row_index|
+        uphill_array = single_uphill_diagonal(row_index, 0, node_arrays)
+        uphill_diagonal_major.push(uphill_array.freeze)
+      end
+
+      # When we reach the last row, pivot and start going right
+      (1...node_arrays.last.length).each do |column_index|
+        uphill_array = single_uphill_diagonal(node_arrays.length - 1, column_index, node_arrays)
+        uphill_diagonal_major.push(uphill_array.freeze)
+      end
+
+      uphill_diagonal_major.freeze
+    end
+
+    def derive_downhill_diagonal_major(node_arrays)
+      downhill_diagonal_major = []
+
+      # From top-right corner of the table (array), go left
+      (0...node_arrays.first.length).to_a.reverse.each do |column_index|
+        downhill_array = single_downhill_diagonal(0, column_index, node_arrays)
+        downhill_diagonal_major.push(downhill_array.freeze)
+      end
+
+      # When we reach the top-left corner, go down.
+      (1...node_arrays.length).each do |row_index|
+        downhill_array = single_downhill_diagonal(row_index, 0, node_arrays)
+        downhill_diagonal_major.push(downhill_array.freeze)
+      end
+
+      downhill_diagonal_major.freeze
+    end
+
+    private
+
+    def single_uphill_diagonal(row_index, column_index, node_arrays)
+      uphill_array = []
+
+      while row_index >= 0 && column_index < node_arrays[row_index].length
+        uphill_array.push(node_arrays[row_index][column_index])
+        row_index -= 1
+        column_index += 1
+      end
+
+      uphill_array
+    end
+
+    def single_downhill_diagonal(row_index, column_index, node_arrays)
+      downhill_array = []
+
+      while row_index < node_arrays.length && column_index < node_arrays[row_index].length
+        downhill_array.push(node_arrays[row_index][column_index])
+        row_index += 1
+        column_index += 1
+      end
+
+      downhill_array
+    end
+  end
+end
---
+++ spec/grid_spec.rb
@@ -0,0 +1,172 @@
+# frozen_string_literal: true
+
+require_relative '../lib/grid'
+
+describe Grid do
+  subject(:row_major) do
+    described_class.build_row_major(initial_value, rows_count, columns_count)
+  end
+
+  let(:initial_value) { 'X' }
+  let(:rows_count) { 6 }
+  let(:columns_count) { 7 }
+
+  before do
+    node_array = Array.new(columns_count, initial_value)
+    allow(NodeArray).to receive(:new).with(columns_count).and_return(node_array)
+  end
+
+  describe '.build_row_major' do
+    context 'when the 2-dimensional Array is returned' do
+      it 'has 6 rows' do
+        expect(row_major.length).to eq(rows_count)
+      end
+
+      it 'is frozen' do
+        expect(row_major).to be_frozen
+      end
+    end
+
+    context 'with each NodeArray in the returned Array' do
+      it 'consists of 7 elements' do
+        all_rows_length = row_major.map(&:length)
+        expect(all_rows_length).to all(eq(columns_count))
+      end
+
+      it 'is frozen' do
+        expect(row_major).to all(be_frozen)
+      end
+
+      it 'has the correct initial values' do
+        all_values = row_major.map(&:join)
+        expected_value = initial_value * columns_count
+        expect(all_values).to all(eq(expected_value))
+      end
+    end
+  end
+
+  describe '.derive_column_major' do
+    subject(:column_major) do
+      described_class.derive_column_major(row_major)
+    end
+
+    context 'when the transposed 2-dimensional Array is returned' do
+      it 'has 7 columns' do
+        expect(column_major.length).to eq(columns_count)
+      end
+
+      it 'is frozen' do
+        expect(column_major).to be_frozen
+      end
+    end
+
+    context 'with each NodeArray in the returned transposed Array' do
+      it 'consists of 6 elements' do
+        all_columns_length = column_major.map(&:length)
+        expect(all_columns_length).to all(eq(rows_count))
+      end
+
+      it 'is frozen' do
+        expect(column_major).to all(be_frozen)
+      end
+
+      it 'has the correct initial values' do
+        all_values = column_major.map(&:join)
+        expected_value = initial_value * rows_count
+        expect(all_values).to all(eq(expected_value))
+      end
+    end
+  end
+
+  describe '.derive_uphill_diagonal_major' do
+    subject(:uphill_diagonal_major) do
+      described_class.derive_uphill_diagonal_major(input_arrays)
+    end
+
+    let(:input_arrays) do
+      [%w[A1 B2 C3 D4 E5 F6 G7].freeze,
+       %w[B1 C2 D3 E4 F5 G6 H7].freeze,
+       %w[C1 D2 E3 F4 G5 H6 I7].freeze,
+       %w[D1 E2 F3 G4 H5 I6 J7].freeze,
+       %w[E1 F2 G3 H4 I5 J6 K7].freeze,
+       %w[F1 G2 H3 I4 J5 K6 L7].freeze].freeze
+    end
+
+    let(:expected_arrays) do
+      [%w[A1].freeze,
+       %w[B1 B2].freeze,
+       %w[C1 C2 C3].freeze,
+       %w[D1 D2 D3 D4].freeze,
+       %w[E1 E2 E3 E4 E5].freeze,
+       %w[F1 F2 F3 F4 F5 F6].freeze,
+       %w[G2 G3 G4 G5 G6 G7].freeze,
+       %w[H3 H4 H5 H6 H7].freeze,
+       %w[I4 I5 I6 I7].freeze,
+       %w[J5 J6 J7].freeze,
+       %w[K6 K7].freeze,
+       %w[L7].freeze].freeze
+    end
+
+    context 'when the uphill diagonal major arrays is returned' do
+      it 'is equal to the expected uphill diagonal array' do
+        is_expected.to eq(expected_arrays)
+      end
+
+      it 'is frozen' do
+        is_expected.to be_frozen
+      end
+    end
+
+    context 'with each NodeArray in the returned uphill diagonal Array' do
+      it 'is frozen' do
+        is_expected.to all(be_frozen)
+      end
+    end
+  end
+
+  describe '.derive_downhill_diagonal_major' do
+    subject(:downhill_diagonal_major) do
+      described_class.derive_downhill_diagonal_major(input_arrays)
+    end
+
+    let(:input_arrays) do
+      [%w[G1 F1 E1 D1 C1 B1 A1].freeze,
+       %w[H2 G2 F2 E2 D2 C2 B2].freeze,
+       %w[I3 H3 G3 F3 E3 D3 C3].freeze,
+       %w[J4 I4 H4 G4 F4 E4 D4].freeze,
+       %w[K5 J5 I5 H5 G5 F5 E5].freeze,
+       %w[L6 K6 J6 I6 H6 G6 F6].freeze].freeze
+    end
+
+    let(:expected_arrays) do
+      [%w[A1].freeze,
+       %w[B1 B2].freeze,
+       %w[C1 C2 C3].freeze,
+       %w[D1 D2 D3 D4].freeze,
+       %w[E1 E2 E3 E4 E5].freeze,
+       %w[F1 F2 F3 F4 F5 F6].freeze,
+       %w[G1 G2 G3 G4 G5 G6].freeze,
+       %w[H2 H3 H4 H5 H6].freeze,
+       %w[I3 I4 I5 I6].freeze,
+       %w[J4 J5 J6].freeze,
+       %w[K5 K6].freeze,
+       %w[L6].freeze].freeze
+    end
+
+    context 'when the downhill diagonal major arrays is returned' do
+      it 'is equal to the expected downhill diagonal array' do
+        is_expected.to eq(expected_arrays)
+      end
+
+      it 'is frozen' do
+        is_expected.to be_frozen
+      end
+    end
+
+    context 'with each NodeArray in the returned downhill diagonal Array' do
+      it 'is frozen' do
+        is_expected.to all(be_frozen)
+      end
+    end
+  end
+end
